df4759926c7b89c7a795eb6b4d2887bf7bc8ec08
Melina <melina.dmec@gmail.com>
2026-03-26 15:34:14 +0100

    ejercicio 1

---
+++ ejercicio1.js
@@ -0,0 +1,26 @@
+// 1. Contar del 1 al 100 de uno en uno
+
+
+//  for 
+console.log("for");
+for (let i = 1; i <= 100; i++) {
+  console.log(i);
+}
+
+//  while 
+console.log("while");
+let i = 1;
+while (i <= 100) {
+  console.log(i);
+  i++;
+}
+
+// do...while 
+console.log("do...while");
+let j = 1;
+do {
+  console.log(j);
+  j++;
+} while (j <= 100);
+
+
