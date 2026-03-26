247da6a27df9059302601e028b4386472e44afd1
Melina <melina.dmec@gmail.com>
2026-03-26 15:46:59 +0100

    ejercicio 2

---
+++ ejercicio2.js
@@ -0,0 +1,27 @@
+// 2. Dibujar figuras con asteriscos
+
+const lado = 4; 
+const contenedor = document.getElementById("figuras");
+
+// CUADRADO
+let cuadrado = "CUADRADO\n";
+for (let fila = 1; fila <= lado; fila++) {
+  let linea = "";
+  for (let col = 1; col <= lado; col++) {
+    linea += "*";
+  }
+  cuadrado += linea + "\n";
+}
+contenedor.innerHTML += "<pre>" + cuadrado + "</pre>";
+
+// TRIÁNGULO
+let triangulo = "TRIÁNGULO\n";
+for (let fila = 1; fila <= lado; fila++) {
+  let linea = "";
+  for (let col = 1; col <= fila; col++) {
+    linea += "*";
+  }
+  triangulo += linea + "\n";
+}
+contenedor.innerHTML += "<pre>" + triangulo + "</pre>";
+
